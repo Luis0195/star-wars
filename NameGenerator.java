f9595c775f8d239f448f15d998492c75eb9b5e1d
Luis Martínez Alonso <luis0pk0rgmail.com>
2021-02-25 09:10:07 +0100

    commit inicial v2

--- NameGenerator.java
+++ NameGenerator.java
@@ -7,12 +7,12 @@
  */
 public class NameGenerator
 {
-    public static final int modApellido1 = 4;
-    public static final int modNombre = 3;
-    public static final int modApellido2 = 2;
-    public static final int modCiudad = 5;
+    public static final int MODAPELLIDO1 = 4;
+    public static final int MODNOMBRE = 3;
+    public static final int MODAPELLIDO2 = 2;
+    public static final int MODCIUDAD = 5;
     
     public void generateStarWarsName(String nombre, String apellido1, String apellido2, String ciudad) {
-        System.out.println(apellido1.substring(0, modApellido1) + nombre.substring(0, modNombre) + " " +  apellido2.substring(0, modApellido2) + ciudad.substring(0, modCiudad));
+        System.out.println(apellido1.substring(0, MODAPELLIDO1) + nombre.substring(0, MODNOMBRE) + " " +  apellido2.substring(0, MODAPELLIDO2) + ciudad.substring(0, MODCIUDAD));
     }
 }
